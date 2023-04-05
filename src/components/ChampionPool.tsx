157f67b680d5e3bb3b6b0c0aa7bcd944aaa1161d
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-04-05 15:06:45 +0200

    first commit

---
+++ src/components/ChampionPool.tsx
@@ -0,0 +1,27 @@
+import Image from 'next/image'
+import { characters } from '../characters'
+
+export default function ChampionPool() {
+    const characterImgs = []
+    for (const character of characters) {
+      for (const role of Object.values(character)) {
+        for (const image of Object.values(role)) {
+          /* if (image.endsWith(".png")) { */
+            characterImgs.push(image)
+          /* } */
+        }
+      }
+    }
+
+    return (
+      <div className="">
+          <div className='grid grid-rows-2 grid-flow-col gap-4 w-96'>
+            {characterImgs.map((characterImg, idx) => (
+              <div key={idx} draggable={true} className='cursor-grab'>
+                {characterImg ? <Image src={characterImg} alt="character img" width={100} height={100} /> : <></>}
+              </div>
+              ))}
+          </div>
+      </div>
+    )
+}
