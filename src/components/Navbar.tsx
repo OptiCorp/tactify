3f04a6ea190361e91b6720f1fcd19d2737acca28
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-04-06 15:11:40 +0200

    moving navLinks array and MobileModal component out of Navbar component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,7 @@
 import Link from "next/link"
 import { useState } from "react"
 
-
-
-
-
-function Navbar() {
-  const navLinks = [
+const navLinks = [
     {
       name: "Home",
       path: "/"
@@ -20,7 +15,7 @@ function Navbar() {
     }
   ]
 
-  function MobileModal() {
+function MobileModal() {
     return (
         <div className="border-4 border-gray-700 rounded absolute top-16 right-5 md:hidden z-50">
             {navLinks.map((link, idx) => (
@@ -36,42 +31,44 @@ function Navbar() {
     )
 }
 
+
+function Navbar() {
   const [showMobileModal, setShowMobileModal] = useState(false)
-return (
-  <nav className="bg-white border-gray-200 dark:bg-gray-900">
-    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-      <Link href={"/"} legacyBehavior>
-        <a className="flex items-center">
+  return (
+      <nav className="bg-white border-gray-200 dark:bg-gray-900">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <Link href={"/"} legacyBehavior>
+            <a className="flex items-center">
+                
+                <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">TACTIFY</span>
+            </a>
+          </Link>
+          <button onClick={() => setShowMobileModal(prevState => !prevState)} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+            <span className="sr-only">Open main menu</span>
+            <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+          </button>
+          {navLinks.map((link, idx) => (
+            <div key={idx} className="hidden w-full md:block md:w-auto" id="navbar-default">
+                <ul  className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                  <li>
+                    <Link href={link.path} className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">{link.name}</Link>
+                  </li>
+                
+                {/* <li>
+                  <Link href={"/comunity-page"} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Comunity Page</Link>
+                </li>
+                <li>
+                  <Link href={"/create-comp-page"} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Create Comp</Link>
+                </li> */}
+              </ul>
+              
             
-            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">TACTIFY</span>
-        </a>
-      </Link>
-      <button onClick={() => setShowMobileModal(prevState => !prevState)} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-        <span className="sr-only">Open main menu</span>
-        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
-      </button>
-      {navLinks.map((link, idx) => (
-        <div key={idx} className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul  className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-              <li>
-                <Link href={link.path} className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500" aria-current="page">{link.name}</Link>
-              </li>
-            
-            {/* <li>
-              <Link href={"/comunity-page"} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Comunity Page</Link>
-            </li>
-            <li>
-              <Link href={"/create-comp-page"} className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Create Comp</Link>
-            </li> */}
-          </ul>
-          
-        
+            </div>
+          ))}
+          {showMobileModal ? <MobileModal /> : null}
         </div>
-      ))}
-      {showMobileModal ? <MobileModal /> : null}
-    </div>
-  </nav> 
-  )
+      </nav> 
+    )
 }
 
 export default Navbar
