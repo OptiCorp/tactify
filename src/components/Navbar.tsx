fde441be3bea4df9643f88701d9a7ad4c9ae5b45
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-04-06 16:15:55 +0200

    clicking outside of mobileModal makes it go away now

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link"
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react"
 
 const navLinks = [
     {
@@ -15,25 +15,41 @@ const navLinks = [
     }
   ]
 
-function MobileModal() {
-    return (
-        <div className="border-4 border-gray-700 rounded absolute top-16 right-5 md:hidden z-50">
-            {navLinks.map((link, idx) => (
-              <ul key={link.name}>
-                <li>
-                  <Link href={link.path} className="block py-2 pl-3 pr-4 text-black bg-white  " aria-current="page">{link.name}
-                  </Link>
-                </li>
-              </ul>
-            ))
-            }
-        </div>
-    )
+
+
+function MobileModal({setShowMobileModal} : { setShowMobileModal : (value: boolean) => void}) {
+  
+  const modalRef = useRef(null)
+  
+  useEffect(() => {
+    function onClick(e: MouseEvent) {
+      if (e.target !== modalRef.current) {
+        setShowMobileModal(false)
+      }
+    }
+    document.addEventListener("click", onClick)
+    return () => document.removeEventListener("click", onClick)
+  }, [])
+  return (
+      <div ref={modalRef} className="border-4 border-gray-700 rounded absolute top-16 right-5 md:hidden z-50">
+          {navLinks.map((link, idx) => (
+            <ul key={link.name}>
+              <li>
+                <Link href={link.path} className="block py-2 pl-3 pr-4 text-black bg-white  " aria-current="page">
+                  {link.name}
+                </Link>
+              </li>
+            </ul>
+          ))
+          }
+      </div>
+  )
 }
 
 
 function Navbar() {
   const [showMobileModal, setShowMobileModal] = useState(false)
+
   return (
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
@@ -43,7 +59,10 @@ function Navbar() {
                 <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">TACTIFY</span>
             </a>
           </Link>
-          <button onClick={() => setShowMobileModal(prevState => !prevState)} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+          <button onClick={(e) => {
+              e.stopPropagation()
+              setShowMobileModal(prevState => !prevState)
+            }} data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
             <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
           </button>
@@ -65,7 +84,7 @@ function Navbar() {
             
             </div>
           ))}
-          {showMobileModal ? <MobileModal /> : null}
+          {showMobileModal ? <MobileModal setShowMobileModal={setShowMobileModal} /> : null}
         </div>
       </nav> 
     )
