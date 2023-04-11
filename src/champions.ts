7ae010ad6570a0db26b69a0b86ab8167ba390a8a
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-04-11 15:53:07 +0200

    using a array to champion info instead of 'api'

---
+++ src/champions.ts
@@ -0,0 +1,366 @@
+
+export const champions = [
+    {
+        alistar: {
+            name: "Alistar",
+            cost: 3,
+            traits: ["Ox Force", "Mascot", "Aegis"],
+            img: "champions/alistar.png"
+        },
+        annie: {
+            name: "Annie",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/annie.png"
+        },
+        ashe: {
+            name: "Ashe",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/ashe.png"
+        },
+        aurelionsol: {
+            name: "Aurelion Sol",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/aurelionsol.png"
+        },
+        belveth: {
+            name: "Belveth",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/belveth.png"
+        },
+        blitzcrank: {
+            name: "Blitzcrank",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/blitzcrank.png"
+        },
+        camille: {
+            name: "Camille",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/camille.png"
+        },
+        draven: {
+            name: "Draven",
+            cost: 3,
+            traits: ["Ox Force", "Mascot", "Aegis"],
+            img: "champions/draven.png"
+        },
+        ekko: {
+            name: "Ekko",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/ekko.png"
+        },
+        ezreal: {
+            name: "Ezreal",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/ezreal.png"
+        },
+        fiddlesticks: {
+            name: "Fiddlesticks",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/fiddlesticks.png"
+        },
+        fiora: {
+            name: "Fiora",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/fiora.png"
+        },
+        gankplank: {
+            name: "Gangplank",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/gangplank.png"
+        },
+        garen: {
+            name: "Garen",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/garen.png"
+        },
+        gnar: {
+            name: "Gnar",
+            cost: 3,
+            traits: ["Ox Force", "Mascot", "Aegis"],
+            img: "champions/gnar.png"
+        },
+        janna: {
+            name: "Janna",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/janna.png"
+        },
+        jax: {
+            name: "Jax",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/jax.png"
+        },
+        jhin: {
+            name: "Jhin",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/jhin.png"
+        },
+        jinx: {
+            name: "Jinx",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/jinx.png"
+        },
+        kaisa: {
+            name: "Kaisa",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/kaisa.png"
+        },
+        kayle: {
+            name: "Kayle",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/kayle.png"
+        },
+        leblanc: {
+            name: "LeBlanc",
+            cost: 3,
+            traits: ["Ox Force", "Mascot", "Aegis"],
+            img: "champions/leblanc.png"
+        },
+        leesin: {
+            name: "Lee Sin",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/leesin.png"
+        },
+        leona: {
+            name: "Leona",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/leona.png"
+        },
+        lucian: {
+            name: "Lucian",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/lucian.png"
+        },
+        lulu: {
+            name: "Lulu",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/lulu.png"
+        },
+        lux: {
+            name: "Lux",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/lux.png"
+        },
+        malphite: {
+            name: "Malphite",
+            cost: 1,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/malphite.png"
+        },
+        missfortune: {
+            name: "Miss Fortune",
+            cost: 3,
+            traits: ["Ox Force", "Mascot", "Aegis"],
+            img: "champions/missfortune.png"
+        },
+        mordekaiser: {
+            name: "Mordekaiser",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/mordekaiser.png"
+        },
+        morgana: {
+            name: "Morgana",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/morgana.png"
+        },
+        nasus: {
+            name: "Nasus",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/nasus.png"
+        },
+        neeko: {
+            name: "Neeko",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/neeko.png"
+        },
+        nilah: {
+            name: "Nilah",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/nilah.png"
+        },
+        nunu: {
+            name: "Nunu",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/nunu.png"
+        },
+        pantheon: {
+            name: "Pantheon",
+            cost: 3,
+            traits: ["Ox Force", "Mascot", "Aegis"],
+            img: "champions/pantheon.png"
+        },
+        poppy: {
+            name: "Poppy",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/poppy.png"
+        },
+        pyke: {
+            name: "Pyke",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/pyke.png"
+        },
+        rammus: {
+            name: "Rammus",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/rammus.png"
+        },
+        rell: {
+            name: "Rell",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/rell.png"
+        },
+        renekton: {
+            name: "Renekton",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/renekton.png"
+        },
+        riven: {
+            name: "Riven",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/riven.png"
+        },
+        samira: {
+            name: "Samira",
+            cost: 3,
+            traits: ["Ox Force", "Mascot", "Aegis"],
+            img: "champions/samira.png"
+        },
+        shen: {
+            name: "Shen",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/shen.png"
+        },
+        sivir: {
+            name: "Sivir",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/sivir.png"
+        },
+        sona: {
+            name: "Sona",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/sona.png"
+        },
+        sylas: {
+            name: "Sylas",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/sylas.png"
+        },
+        syndra: {
+            name: "Syndra",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/syndra.png"
+        },
+        twistedFate: {
+            name: "Twisted Fate",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/twisted-fate.png"
+        },
+        ultEzreal: {
+            name: "Ultimate Ezreal",
+            cost: 3,
+            traits: ["Ox Force", "Mascot", "Aegis"],
+            img: "champions/ultimateezreal.png"
+        },
+        urgot: {
+            name: "Urgot",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/urgot.png"
+        },
+        vayne: {
+            name: "Vayne",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/vayne.png"
+        },
+        vex: {
+            name: "Vex",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/vex.png"
+        },
+        vi: {
+            name: "Vi",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/vi.png"
+        },
+        viego: {
+            name: "Viego",
+            cost: 2,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/viego.png"
+        },
+        warwick: {
+            name: "Warwick",
+            cost: 1,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/warwick.png"
+        },
+        wukong: {
+            name: "Wukong",
+            cost: 1,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/wukong.png"
+        },
+        yasuo: {
+            name: "Yasuo",
+            cost: 1,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/yasuo.png"
+        },
+        zac: {
+            name: "Zac",
+            cost: 1,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/zac.png"
+        },
+        aatrox: {
+            name: "Aatrox",
+            cost: 1,
+            traits: ["Ox Force", "GadgetTeen", "Spellslinger"],
+            img: "champions/aatrox.png"
+        },
+        
+    }
+]
