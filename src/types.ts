641c4d4e6d6853ed711ba367b05d8831e745535d
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-04-13 08:39:49 +0200

    removing types, thats not used

--- src/types.ts
+++ src/types.ts
@@ -1,27 +1,6 @@
-export type Character = {
-    tank?: {
-      tank1: string
-      tank2: string
-      tank3: string
-    };
-    dps?: {
-      dps1: string
-    }, 
-    caster?: {
-      caster1: string
-      caster2: string
-      caster3: string
-      caster4: string
-    }
-  }
-
-
 export type TChampion = {
   titles?: string[];
   images?: string;
   tags?: string[];
 }
 
-export type Props = {
-    margin : string
-}
