3ad4109faade894d4a6741f129ce527ff87a8fc9
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-04-20 08:41:00 +0200

    better styling

--- src/components/BoardInfoPanel.tsx
+++ src/components/BoardInfoPanel.tsx
@@ -4,6 +4,7 @@ export function BoardInfoPanel(props: { tileMapValue: any | undefined }) {
   const traitCount: Record<string, number> = {};
   const countedChampion: Record<string, boolean> = {};
 
+  //! Bug: duplicate champs on the board increments trait count
   for (const tile of Object.values(props.tileMapValue)) {
     if (tile) {
       const champion = champions.find((champ) => champ.name === tile);
@@ -27,23 +28,15 @@ export function BoardInfoPanel(props: { tileMapValue: any | undefined }) {
     .sort((a, b) => b.count - a.count);
 
   return (
-    <div className="ml-4 h-20  w-full overflow-y-scroll  p-2 md:h-72  md:w-24 md:min-w-[165px]">
-      {/* {Object.entries(traitCount)
-        .sort((a: number, b: number) => a - b)
-        .map(([trait, count]) => (
-          <div key={trait} className="flex gap-1">
-            <p>{count}</p>
-            <p>{trait}</p>
-          </div>
-        ))} */}
+    <div className="ml-4 h-20  w-44 overflow-y-auto  p-2 md:h-72  md:w-24 md:min-w-[165px]">
       {sortedTraitsByCount.map(({ trait, count }, idx) => (
         <div
           key={idx}
           className="mb-2 flex items-center gap-1 border bg-orange-300 p-2 text-xs"
         >
           <img
-            className="h-4 w-4 invert"
-            src={`traits/${trait.replace(/[\s:]/g, '')}.png`}
+            className="h-3 w-3 invert md:h-4 md:w-4"
+            src={`traits/${trait.replace(/[\s:.]/g, '')}.png`}
           />
 
           <span className="bg-orange-400 p-1">{count}</span>
