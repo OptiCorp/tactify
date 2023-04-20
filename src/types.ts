bb065f7eb4fb8c1378a85a1b09872884b0000a39
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-04-20 08:38:38 +0200

    adding new types

--- src/types.ts
+++ src/types.ts
@@ -4,3 +4,29 @@ export type TChampion = {
   images?: string;
   traits?: string[];
 };
+
+export type CharacterType = {
+  name: string;
+  image: string;
+  gender: string;
+  species: string;
+  origin: string;
+  cost: Cost;
+  id?: number;
+};
+
+export type SelectedType = 'cost' | 'name' | 'search';
+
+export type Props = {
+  selectedImage: string;
+  setSelectedImage: () => void;
+};
+
+export enum Cost {
+  Human = 1,
+  Alien,
+  unknown,
+  Animal,
+  Robot,
+  Mythological,
+}
