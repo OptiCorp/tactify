0d608de73d36807350ebaab458e607c270d2dcb4
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-04-20 08:41:58 +0200

    Updating BoardInfoPanel for an api

---
+++ src/components/BoardInfoPanelApi.tsx
@@ -0,0 +1,48 @@
+import { CharacterType } from '../types';
+
+function BoardInfoPanelApi({
+  characterData,
+  tileMapValue,
+}: {
+  tileMapValue: object;
+  characterData: CharacterType[];
+}) {
+  const speciesCount: Record<string, number> = {};
+
+  //! Bug: duplicate characters on the board increments trait count
+  for (const tile of Object.values(tileMapValue)) {
+    if (tile) {
+      const characters = characterData.find(
+        (character) => character.image === tile
+      );
+      if (characters) {
+        const { species } = characters;
+        if (speciesCount[species]) {
+          speciesCount[species]++;
+        } else {
+          speciesCount[species] = 1;
+        }
+      }
+    }
+  }
+
+  const sortedSpeciesByCount = Object.entries(speciesCount)
+    .map(([species, count]) => ({ species, count }))
+    .sort((a, b) => b.count - a.count);
+
+  return (
+    <div className="h-20  w-44 overflow-y-auto  p-2 md:h-72  md:w-24 md:min-w-[165px]">
+      {sortedSpeciesByCount?.map(({ species, count }, idx) => (
+        <div
+          key={idx}
+          className="mb-2 flex items-center gap-1  border border-amber-500 bg-orange-300 bg-opacity-20 p-2 text-xs font-bold text-amber-500"
+        >
+          <span>{count}</span>
+          <span className="w-[120px] truncate">{species}</span>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default BoardInfoPanelApi;
