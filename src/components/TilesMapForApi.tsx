7c70b3d8fc9201585ec030aa890de471b507817b
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-04-21 09:19:36 +0200

    Adding text behind the tiles (chat)

--- src/components/TilesMapForApi.tsx
+++ src/components/TilesMapForApi.tsx
@@ -35,14 +35,47 @@ function TilesMapForApi({
 
   return (
     <>
-      <div className="flex flex-col items-center md:flex-row">
-        <div className="">
+      <div className="font-creepster relative z-10 flex flex-col items-center md:flex-row">
+        <div className="z-0 hidden select-none opacity-50 md:block">
+          {characterData.map(({ name, image }) => {
+            if (name === 'Rick Sanchez') {
+              return (
+                <div key={name}>
+                  <img
+                    src={image}
+                    alt={name}
+                    className="absolute left-28  top-36 h-10 w-10  rounded-full border-2 border-blue-500"
+                  />
+
+                  <p className="absolute left-40 z-0 text-xl  text-white ">
+                    : This isn't Game of Thrones, Morty!
+                  </p>
+                </div>
+              );
+            } else if (name === 'Morty Smith') {
+              return (
+                <div key={name}>
+                  <img
+                    src={image}
+                    alt={name}
+                    className="absolute left-28 top-48  h-10 w-10 rounded-full  border-2 border-blue-500"
+                  />
+
+                  <p className="absolute left-40 top-48 z-0 text-xl  text-white ">
+                    : Aww, gee, you got me there, Rick.
+                  </p>
+                </div>
+              );
+            }
+          })}
+        </div>
+        <div className="z-50">
           <BoardInfoPanelApi
             tileMapValue={tileMapValue}
             characterData={characterData}
           />
         </div>
-        <div className="grid w-[80%] grid-cols-7 gap-4 p-4">
+        <div className="z-50 grid grid-cols-7 gap-4 p-4">
           {Object.entries(tileMapValue).map(([key, value]) => {
             return (
               <div key={key} className="">
@@ -50,7 +83,7 @@ function TilesMapForApi({
                   onClick={() => {
                     getTileValue(key);
                   }}
-                  className="relative flex h-10 w-10 items-center justify-center border border-amber-500 bg-orange-300 bg-opacity-20  md:h-14 md:w-14 "
+                  className="relative flex h-10 w-10 items-center justify-center border border-amber-500 bg-orange-300  bg-opacity-20 md:h-14 md:w-14"
                 >
                   {value ? (
                     <img
