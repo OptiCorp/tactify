c0d7e9e4714e2e42644f19d4a93eb7daa33eedb9
bjornole-kodehode <bjornole.kodehode@gmail.com>
2023-04-25 10:26:14 +0200

    switch => object lookup, toggle for radio button

--- src/components/CharacterContainer.tsx
+++ src/components/CharacterContainer.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 import { getData } from 'api';
-import { CharacterType, Cost, Props, SelectedType } from '../types';
+import { CharacterType, Cost, Props } from '../types';
 import TilesMapForApi from './TilesMapForApi';
 import Searchbar from './Searchbar';
 import CharacterPool from './CharacterPool';
@@ -16,39 +16,21 @@ const URL = 'https://rickandmortyapi.com/api/character?species=';
 const imageUrl =
   'https://cdn.leonardo.ai/users/31ab532b-b9ee-497b-9f7d-674e61cedab3/generations/7e2acbd6-9830-43c0-8a5e-859b1aeb9ce6/Leonardo_Diffusion_A_dark_in_the_ocean_no_lighting_rick_and_mo_1.jpg';
 
-const costCasesMap = {
+const costCasesMap: { [key: string]: Cost } = {
   human: Cost.Human,
   Alien: Cost.Alien,
   unknown: Cost.unknown,
   Animal: Cost.Animal,
   Robot: Cost.Robot,
-  Mythological_Creature: Cost.Mythological,
+  'Mythological Creature': Cost.Mythological,
 };
-// TODO: Implement this?
-/* function getCost(value: string): Cost {
-  const cost = costCasesMap[value];
-
-  return cost;
-} */
 
 function getCost(value: string): Cost {
-  switch (value) {
-    case 'Human':
-      return Cost.Human;
-    case 'Alien':
-      return Cost.Alien;
-    case 'unknown':
-      return Cost.unknown;
-    case 'Animal':
-      return Cost.Animal;
-    case 'Robot':
-      return Cost.Robot;
-    case 'Mythological Creature':
-      return Cost.Mythological;
-    default:
-      return Cost.Human;
-  }
+  const cost = costCasesMap[value];
+
+  return cost || Cost.Human;
 }
+
 // Make a dynamic board?
 /* const obj = {};
 function createObject(numKeys: number, numValues: number) {
@@ -196,16 +178,6 @@ function CharacterContainer() {
   return (
     <div className="ml-2 mr-2 mt-10 flex flex-col items-center justify-center">
       <div className="flex flex-col items-center">
-        {/* <button
-          disabled={isEmpty}
-          className={`mb-5 rounded-sm border border-amber-500 bg-orange-300 bg-opacity-40 px-4 py-2 text-amber-500 hover:bg-opacity-20 ${
-            isEmpty && 'cursor-not-allowed opacity-50'
-          }`}
-          onClick={handleSaveBoard}
-        >
-          Save Board
-        </button> */}
-
         <div
           className="flex gap-4"
           style={{
@@ -244,52 +216,22 @@ function CharacterContainer() {
           >
             <div
               className={`flex h-10 w-10 items-center justify-center rounded-l-md border  hover:border-amber-400 ${
-                sortingType && 'border-r-2 border-amber-500'
+                sortingType && 'border-4  border-amber-500'
               }`}
             >
               <img src="coin.svg" />
             </div>
             <div
               className={`flex h-10 w-10 items-center justify-center rounded-r-md border  hover:border-amber-400 ${
-                !sortingType && 'border-l-2 border-amber-500'
+                !sortingType &&
+                'border-4 border-l-2 border-amber-500 font-extrabold'
               }`}
             >
               <span>A-Z</span>
             </div>
           </div>
-          {/* <div className="flex flex-col items-center gap-2 md:ml-2 md:flex-row">
-            <div className="flex gap-2">
-              <input
-                type="radio"
-                id="cost"
-                name="sorting"
-                checked={sortingType}
-                onChange={() => handleSort()}
-              />
-              <label htmlFor="cost">Cost</label>
-            </div>
-            <div className="flex gap-2">
-              <input
-                id="name"
-                type="radio"
-                checked={!sortingType}
-                name="sorting"
-                onChange={() => handleSort()}
-                className="border-[#0A2342]"
-              />
-              <label htmlFor="name">A-Z</label>
-            </div>
-          </div> */}
+
           <div className="flex items-center gap-2">
-            {/* <button
-              className={`absolute right-4 top-[10%] hidden rounded-sm border border-amber-500 bg-orange-300 bg-opacity-40 px-4 py-2 text-amber-500 hover:bg-opacity-20  md:block ${
-                isDisabled ? 'cursor-not-allowed opacity-50' : ''
-              }`}
-              disabled={isDisabled}
-              onClick={() => clearBoard()}
-            >
-              Clear Board
-            </button> */}
             <div className="ml-4">
               <CharacterCost characterData={characterData} />
             </div>
@@ -306,7 +248,7 @@ function CharacterContainer() {
         </div>
         <div>
           {loading ? (
-            <div className="flex min-h-[208px] min-w-[338px] flex-col items-center justify-center border border-t-0 border-amber-500 bg-[#22272e] md:min-w-[750px] lg:min-w-[978px]">
+            <div className="flex min-h-[208px] min-w-[338px] flex-col items-center justify-center border border-t-0 border-amber-500 bg-[#22272e] md:min-w-[750px] lg:min-w-[993px]">
               <BarLoader color="#F59E0B" />
             </div>
           ) : (
